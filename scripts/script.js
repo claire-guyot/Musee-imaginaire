b5c34a50dc369cf53b5a127a7b3c35a501a0a032
claire-guyot <claire.guyot@gmx.com>
2018-01-04 11:37:26 +0100

    scripts
    ouverture de pages sans en créer
    début d'actions sur les catégories

---
+++ scripts/script.js
@@ -0,0 +1,96 @@
+
+  var checkSecret = document.querySelector('#secret');
+
+
+  if (checkSecret !== null) {
+    checkSecret.addEventListener('click', function() {
+      if(checkSecret.checked){
+        alert("checkSecret checked");
+      }
+      else {
+        alert("checkSecret unchecked");
+
+      }
+    });
+  }
+
+  var checkImaginaire = document.querySelector('#imaginaire');
+
+
+  if (checkImaginaire !== null) {
+    checkImaginaire.addEventListener('click', function() {
+    alert("checkImaginaire clicked");
+    });
+  }
+
+  var checkAvenir = document.querySelector('#avenir');
+
+
+  if (checkAvenir !== null) {
+    checkAvenir.addEventListener('click', function() {
+    alert("checkAvenir clicked");
+    });
+  }
+
+  var checkEnfance = document.querySelector('#enfance');
+
+
+  if (checkEnfance !== null) {
+    checkEnfance.addEventListener('click', function() {
+    alert("checkEnfance clicked");
+    });
+  }
+
+  var checkCiel = document.querySelector('#ciel');
+
+
+  if (checkCiel !== null) {
+    checkCiel.addEventListener('click', function() {
+    alert("checkCiel clicked");
+    });
+  }
+
+  var checkHistoire = document.querySelector('#histoire');
+
+
+  if (checkHistoire !== null) {
+    checkHistoire.addEventListener('click', function() {
+    alert("checkHistoire clicked");
+    });
+  }
+
+  var checkMonde = document.querySelector('#monde');
+
+
+  if (checkMonde !== null) {
+    checkMonde.addEventListener('click', function() {
+    alert("checkMonde clicked");
+    });
+  }
+
+  var checkAventure = document.querySelector('#aventure');
+
+
+  if (checkAventure !== null) {
+    checkAventure.addEventListener('click', function() {
+    alert("checkAventure clicked");
+    });
+  }
+
+  var checkEtrange = document.querySelector('#etrange');
+
+
+  if (checkEtrange !== null) {
+    checkEtrange.addEventListener('click', function() {
+    alert("checkEtrange clicked");
+    });
+  }
+
+  var checkVrai = document.querySelector('#vrai');
+
+
+  if (checkVrai !== null) {
+    checkVrai.addEventListener('click', function() {
+    alert("checkVrai clicked");
+    });
+  }
